895a3341e8df2ca9b1d058b0a1906d5aeaf37787
Rainier Barrett <rbarret8@ur.rochester.edu>
2017-09-29 11:28:45 -0400

    Updated do_ROC.py to take more args, works better now

--- do_ROC.py
+++ do_ROC.py
@@ -5,16 +5,19 @@ import math
 import sys
 
 def printHelp():
-    print("do_ROC.py [file_locations] [max_number_clusters]")
+    print("do_ROC.py [file_locations] [min_number_clusters] [max_number_clusters] [weight_for_FPR_dist]")
 
-if len(sys.argv) is not 3:
+if len(sys.argv) is not 5:
     printHelp()
     exit()
 
 dirname = sys.argv[1]
-num_clusters = sys.argv[2]
+min_num_clusters = sys.argv[2]
+max_num_clusters = sys.argv[3]
+prefactor = float(sys.argv[4])
 
-for i in range(2, int(num_clusters)+1):
+
+for i in range(int(min_num_clusters), int(max_num_clusters)+1):
     fprfile = dirname+"/{}_clusters_FPR.txt".format(i)
     tprfile = dirname+"/{}_clusters_TPR.txt".format(i)
     fpr_arr = np.genfromtxt(fprfile)
@@ -23,7 +26,7 @@ for i in range(2, int(num_clusters)+1):
     best_idx = 0
     old_dist = 2
     for j in range(1,len(fpr_arr)):
-        dist = math.sqrt(1.5 * fpr_arr[j] **2 + (1-tpr_arr[j]) **2)
+        dist = math.sqrt(prefactor*fpr_arr[j] **2 + (1-tpr_arr[j]) **2)
         if(old_dist > dist):
             best_idx = j
             old_dist = dist
@@ -40,3 +43,8 @@ for i in range(2, int(num_clusters)+1):
     plt.plot(fpr_arr, fpr_arr, label='totally random', ls=':', color='black')
     plt.legend(loc='best')
     plt.savefig(plotname)
+
+    with open('{}/{}_clusters_ROC_log.txt'.format(dirname, i), 'w+') as f:
+        f.write('best idx {}\n'.format(best_idx))
+        f.write('best FPR {}\n'.format(fpr_arr[best_idx]))
+        f.write('best TPR {}\n'.format(tpr_arr[best_idx]))
